769d8fbeb56f16e58ea19d591af9d91b9b8e2b67
ucdungo <yunildungo@gmail.com>
2025-02-26 21:45:13 +0800

    02-26-2025

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 <x-layout>
     <div class="col-md-12 main-content">
         <div class="col-md-12 content-header">
-            <h3>Home page</h3>
-            <button type="button" class="btn btn-primary">Button</button>
+            <h3>List of Products</h3>
+            <button type="button" class="btn btn-primary">Add Product</button>
         </div>
         <div class="col-md-12 content-body">
-            <h1>This is home page.</h1>        
         </div>
     </div>
 </x-layout>
