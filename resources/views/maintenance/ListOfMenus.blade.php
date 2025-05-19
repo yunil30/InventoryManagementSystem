fbf1145296cb2a29cd149823b0a85fb3c3a5545c
ucdungo <yunildungo@gmail.com>
2025-05-19 20:56:21 +0800

    05-19-2025

--- resources/views/maintenance/ListOfMenus.blade.php
+++ resources/views/maintenance/ListOfMenus.blade.php
@@ -296,6 +296,7 @@
         const menuType = document.getElementById('showMenuType');
         const parentMenu = document.getElementById('showParentMenu');
         const menuIndex = document.getElementById('showMenuIndex');
+        const menuIndex = document.getElementById('showMenuIndex');
     
         $.ajax({
             url: `/GetMenuRecord`,
